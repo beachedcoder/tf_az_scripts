952a2c869bb1af524d663335355f4fd167bcd24a
Nathan <beachcoder@outlook.com>
2021-02-15 06:46:13 -0500

    removed echo

--- install_space_invaders.sh
+++ install_space_invaders.sh
@@ -1,12 +1,7 @@
 echo "updating packages...."
 apt update
-echo "finished updating. Now installing packages"
 apt install -y git apache2
-echo "finished installing git and apache2"
 cd /var/www/html
 rm index.html -f
-echo "pulling web application"
 git init
-echo "initialized current directory with git"
 git pull https://github.com/drehnstrom/space-invaders
-echo "finished pulling repository to local virtual machine"
