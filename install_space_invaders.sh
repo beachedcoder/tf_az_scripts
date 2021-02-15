a22aef77a408b075ab80922a896cf3cdd3d1faf9
Nathan <beachcoder@outlook.com>
2021-02-15 06:25:58 -0500

    updated file with comments
    added echo statements to log actions

--- install_space_invaders.sh
+++ install_space_invaders.sh
@@ -1,6 +1,10 @@
+echo "updating packages...."
 apt update
+echo "finished updating. Now installing packages"
 apt install -y git apache2
+echo "finished installing git and apache2"
 cd /var/www/html
 rm index.html -f
+echo "pulling web application"
 git init
 git pull https://github.com/drehnstrom/space-invaders
